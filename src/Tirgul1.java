ee40099d4c487868869d958f5065a17aa0c61e12
yed1dya <yevenchen@Gmail.com>
2025-10-25 18:54:59 +0300

    tirgul 2

--- src/Tirgul1.java
+++ src/Tirgul1.java
@@ -1,5 +1,104 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Tirgul1 {
     public static void main(String[] args) {
+        problem8();
+    }
 
+    public static void problem1(){
+        Scanner scan = new Scanner(System.in);
+        int x;
+        int y;
+        System.out.println("first number");
+        x = scan.nextInt();
+        System.out.println("second number");
+        y = scan.nextInt();
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("swapping...");
+        int t = x;
+        x = y;
+        y = t;
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+    }
+
+    public static void problem2(){
+        Scanner scan = new Scanner(System.in);
+        int x, y;
+        System.out.println("first number");
+        x = scan.nextInt();
+        System.out.println("second number");
+        y = scan.nextInt();
+        int avg = (x + y) / 2;
+        System.out.println("average (rounded): " + avg);
+    }
+
+    public static void problem3(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("first number");
+        float x = scan.nextFloat();
+        System.out.println("second number");
+        float y = scan.nextFloat();
+        float avg = (x + y) / 2;
+        System.out.println("average: " + avg);
+    }
+
+    public static void problem4(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("first number");
+        int x = scan.nextInt();
+        System.out.println("second number");
+        int y = scan.nextInt();
+        float avg = (float) (x + y) / 2;
+        System.out.println("average (exact): " + avg);
+    }
+
+    public static void problem5(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("first number");
+        int x = scan.nextInt();
+        System.out.println("second number");
+        int y = scan.nextInt();
+        System.out.println("remainder of x / y: " + x % y);
+    }
+
+    public static void problem6(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter 3-digit number");
+        int x = scan.nextInt();
+        int sum = x % 10;
+        x /= 10;
+        sum += x % 10;
+        x /= 10;
+        sum += x % 10;
+        System.out.println("digit sum: " + sum);
+    }
+
+    public static void problem7(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter radius");
+        float r = scan.nextFloat();
+        System.out.println("enter C for circumference or A for area");
+        String x = scan.next().toLowerCase();
+        if(x.equals("a")){
+            System.out.println("area: " + (3.14 * r * r));
+        } else if (x.equals("c")) {
+            System.out.println("circumference: " + (3.14 * r * 2));
+        }
+        else {
+            System.out.println("bad input");
+        }
+    }
+
+    public static void problem8() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter number of seconds");
+        int seconds = scan.nextInt();
+        int hours = seconds / 3600;
+        seconds -= hours * 3600;
+        int minutes = seconds / 60;
+        seconds -= minutes * 60;
+        System.out.println(hours + ":" + minutes + ":" + seconds);
     }
 }
---
+++ src/Tirgul2.java
@@ -0,0 +1,163 @@
+import java.util.Scanner;
+
+public class Tirgul2 {
+    public static void main(String[] args) {
+        problem10();
+    }
+
+    public static void problem1a(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter N");
+        int n = scan.nextInt();
+        System.out.println("1 + 2 + ... + N = " + n * (n + 1) / 2);
+    }
+
+    public static void problem1b(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter N");
+        int n = scan.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("1 + 2 + ... + N = " + sum);
+    }
+
+    public static void problem2(){
+        Scanner scan = new Scanner(System.in);
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            System.out.println("enter number " + (i+1));
+            arr[i] = scan.nextInt();
+        }
+        int sum = 0;
+        for (int i : arr){
+            sum += i;
+        }
+        System.out.println("average: " + ((double)sum / 3));
+    }
+
+    public static void problem3(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter N");
+        int n = scan.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+
+    public static void problem4(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter N");
+        long n = scan.nextLong();
+        System.out.println("enter X");
+        int x = scan.nextInt();
+        if (n == 0 && x == 0){
+            System.out.println(1);
+        }
+        else {
+            int count = 0;
+            while (n > 0){
+                if (n % 10 == x){
+                    count++;
+                }
+                n /= 10;
+            }
+            System.out.println(count);
+        }
+    }
+
+    public static void problem5(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter M");
+        int n = scan.nextInt();
+        System.out.println("enter N");
+        int m = scan.nextInt();
+        long res = 1;
+        for (int i = 0; i < m; i++) {
+            res *= n;
+        }
+        System.out.println(res);
+    }
+
+    public static void problem6(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter x");
+        float x = scan.nextFloat();
+        System.out.println("enter y");
+        float y = scan.nextFloat();
+        System.out.println("enter z");
+        float z = scan.nextFloat();
+        float t;
+        if (x > y){
+            t = x; x = y; y = t;
+        }
+        if (y > z){
+            t = y; y = z; z = t;
+        }
+        if (x > y){
+            t = x; x = y; y = t;
+        }
+        System.out.println("min: " + x + ", max: " + z);
+    }
+
+    public static void problem7(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter a");
+        float a = scan.nextFloat();
+        System.out.println("enter b");
+        float b = scan.nextFloat();
+        System.out.println("enter c");
+        float c = scan.nextFloat();
+        double squareRoot = Math.sqrt(b*b - 4*a*c);
+        System.out.println("X1 = " + ((-b - squareRoot) / 2*a));
+        System.out.println("X2 = " + ((-b + squareRoot) / 2*a));
+    }
+
+    public static void problem8(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter N");
+        int n = scan.nextInt();
+        long ans = 1;
+        while (n > 1){
+            ans *= n;
+            n--;
+        }
+        System.out.println(ans);
+    }
+
+    public static void problem9(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter N");
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+        int sum = 0, max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++){
+            System.out.print("enter num: ");
+            arr[i] = scan.nextInt();
+            sum += arr[i];
+            max = Math.max(max, arr[i]);
+        }
+        System.out.println("average: " + ((double)sum / n) + ", max: " + max);
+    }
+
+    public static void problem10(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter string");
+        String s = scan.next();
+        if (s.length() >= 9){
+            if (s.substring(2, 5).equals(s.substring(6, 9))){
+                System.out.println("places 2-4 equal to places 6-8");
+            }
+            else{
+                System.out.println("places 2-4 not equal to places 6-8");
+            }
+        }
+        else{
+            System.out.println("not long enough");
+        }
+    }
+}
